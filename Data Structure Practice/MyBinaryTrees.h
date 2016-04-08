eec7f4c7a8c3e8a6862098b0be7c92968f18e9e8
evilBird <travishenspeter@Traviss-MacBook-Pro.local>
2016-04-08 18:46:30 -0500

    implement binary trees

--- Data Structure Practice/MyBinaryTrees.h
+++ Data Structure Practice/MyBinaryTrees.h
@@ -8,6 +8,11 @@
 
 #import <Foundation/Foundation.h>
 
+#define BinaryTree_Size(tree)   ((tree)->size)
+#define BinaryTree_Root(tree)   ((tree)->root)
+#define BinaryTree_Left(node)   ((node)->left)
+#define BinaryTree_Right(node)  ((node)->right)
+
 @interface MyBinaryTrees : NSObject
 
 typedef struct MyBinaryTreeNode{
@@ -16,9 +21,20 @@ typedef struct MyBinaryTreeNode{
     struct MyBinaryTreeNode *right;
 }BinaryTreeNode;
 
+BinaryTreeNode* BinaryTreeNodeCreate();
+
 typedef struct MyBinaryTree{
+    int             size;
+    int             (*compare)(const void *key1, const void *key2);
     BinaryTreeNode  *root;
 }BinaryTree;
 
+BinaryTree* BinaryTreeCreate();
+void BinaryTreeDestroy(BinaryTree *tree);
+int BinaryTreeInsertLeft(BinaryTree *tree, BinaryTreeNode *node, const void *data);
+int BinaryTreeInsertRight(BinaryTree *tree, BinaryTreeNode *node, const void *data);
+void BinaryTreeRemoveLeft(BinaryTree *tree, BinaryTreeNode *node);
+void BinaryTreeRemoveRight(BinaryTree *tree, BinaryTreeNode *node);
+int BinaryTreeMerge(BinaryTree *merge, BinaryTree *left, BinaryTree *right, const void *data);
 
 @end
--- Data Structure Practice/MyBinaryTrees.m
+++ Data Structure Practice/MyBinaryTrees.m
@@ -10,4 +10,142 @@
 
 @implementation MyBinaryTrees
 
+BinaryTreeNode* BinaryTreeNodeCreate(){
+    BinaryTreeNode *node = (BinaryTreeNode *)malloc(sizeof(BinaryTreeNode));
+    node->left = NULL;
+    node->right = NULL;
+    node->data = NULL;
+    return node;
+}
+
+BinaryTree* BinaryTreeCreate(){
+    BinaryTree *tree = (BinaryTree *)malloc(sizeof(BinaryTree));
+    tree->root = NULL;
+    tree->size = 0;
+    return tree;
+}
+
+
+int BinaryTreeInsertLeft(BinaryTree *tree, BinaryTreeNode *node, const void *data){
+    
+    BinaryTreeNode *newNode,**position;
+    if (node == NULL) {
+        if (BinaryTree_Size(tree) > 0) {
+            return -1;
+        }
+        
+        position = &tree->root;
+    }else{
+        if (BinaryTree_Left(node) != NULL) {
+            return -1;
+        }
+        
+        position = &node->left;
+    }
+    
+    newNode = BinaryTreeNodeCreate();
+    if (newNode == NULL) {
+        return -1;
+    }
+    
+    newNode->data = (void*)data;
+    *position = newNode;
+    tree->size++;
+    return 0;
+}
+
+int BinaryTreeInsertRight(BinaryTree *tree, BinaryTreeNode *node, const void *data){
+    BinaryTreeNode *newNode,**position;
+    if (node == NULL) {
+        if (BinaryTree_Size(tree) > 0) {
+            return -1;
+        }
+        
+        position = &tree->root;
+        
+    }else{
+        if (BinaryTree_Right(node) != NULL) {
+            return -1;
+        }
+        
+        position = &node->right;
+    }
+    
+    newNode = BinaryTreeNodeCreate();
+    
+    if (newNode == NULL) {
+        return -1;
+    }
+    
+    newNode->data = (void*)data;
+    *position = newNode;
+    tree->size++;
+    return 0;
+}
+
+void BinaryTreeRemoveLeft(BinaryTree *tree, BinaryTreeNode *node){
+    
+    BinaryTreeNode **position;
+    if (BinaryTree_Size(tree) == 0) {
+        return;
+    }
+    if (node==NULL) {
+        position = &tree->root;
+    }else{
+        position = &node->left;
+    }
+    
+    if (*position != NULL) {
+        free(*position);
+        *position = NULL;
+    }
+    
+    tree->size--;
+}
+
+void BinaryTreeRemoveRight(BinaryTree *tree, BinaryTreeNode *node){
+    BinaryTreeNode **position;
+    if (BinaryTree_Size(tree) == 0) {
+        return;
+    }
+    if (node==NULL) {
+        position = &tree->root;
+    }else{
+        position = &node->right;
+    }
+    
+    if (*position != NULL) {
+        free(*position);
+        *position = NULL;
+    }
+    
+    tree->size--;
+}
+
+void BinaryTreeDestroy(BinaryTree *tree){
+    BinaryTreeRemoveLeft(tree, NULL);
+    free(tree);
+    tree = NULL;
+}
+
+int BinaryTreeMerge(BinaryTree *merge, BinaryTree *left, BinaryTree *right, const void *data){
+    
+    merge = BinaryTreeCreate();
+    if (BinaryTreeInsertLeft(merge, NULL, data) != 0) {
+        BinaryTreeDestroy(merge);
+        return -1;
+    }
+    BinaryTree_Root(merge)->left = BinaryTree_Root(left);
+    BinaryTree_Root(merge)->right = BinaryTree_Root(right);
+    merge->size = merge->size + BinaryTree_Size(left) + BinaryTree_Size(right);
+    
+    left->root = NULL;
+    left->size = 0;
+    right->root = NULL;
+    right->size = 0;
+    
+    return 0;
+}
+
+
 @end
